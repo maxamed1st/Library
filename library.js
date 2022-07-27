daec3b61b9a1892d82a3736fa79ef8bcd59557d0
maxamed1st <maxamed1st@icloud.com>
2022-07-27 17:42:24 +0200

    create basic library functionality

--- library.js
+++ library.js
@@ -1,2 +1,39 @@
 const books = document.getElementById("books");
 const newBook = document.getElementById("newBook");
+let form = document.querySelector("form");
+
+//Array for bookObjects
+library = []
+
+//create bookobject
+const bookObject = function(title, author, pages){
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.readState = false,
+    //update read state
+    this.read = () => {
+        this.readState = true
+    },
+    //return book info
+    this.info = () => {
+        return [this.title, this.author, this.pages, this.read]
+    }
+}
+//add new bookObject to library
+const addNewBook = function(bookObject) {
+    library.push(bookObject);
+}
+//create new bookObject on submit and update bookArray
+form.submit.addEventListener("click", ()=> {
+    title = form.title.value;
+    author = form.author.value;
+    pages = form.pages.value;
+    readStatus = form.readStatus;
+
+    book = new bookObject(title, author, pages);
+    if (form.readStatus.checked) {
+        book.read();
+    }
+    addNewBook(book);
+})
