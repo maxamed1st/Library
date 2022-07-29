1a0f1ca5d4914680693dfd654f2d957136ac7b25
maxamed1st <maxamed1st@icloud.com>
2022-07-29 19:36:41 +0200

    Append books to DOM

--- library.js
+++ library.js
@@ -1,10 +1,8 @@
-const books = document.getElementById("books");
-const newBook = document.getElementById("newBook");
+let books = document.getElementById("books");
 let form = document.querySelector("form");
 
 //Array for bookObjects
 library = []
-
 //create bookobject
 const bookObject = function(title, author, pages){
     this.title = title,
@@ -17,7 +15,7 @@ const bookObject = function(title, author, pages){
     },
     //return book info
     this.info = () => {
-        return [this.title, this.author, this.pages, this.read]
+        return [this.title, this.author, this.pages, this.readState]
     }
 }
 //add new bookObject to library
@@ -25,15 +23,55 @@ const addNewBook = function(bookObject) {
     library.push(bookObject);
 }
 //create new bookObject on submit and update bookArray
-form.submit.addEventListener("click", ()=> {
+form.submit.addEventListener("click", (e)=> {
     title = form.title.value;
     author = form.author.value;
     pages = form.pages.value;
     readStatus = form.readStatus;
 
-    book = new bookObject(title, author, pages);
+    const book = new bookObject(title, author, pages);
     if (form.readStatus.checked) {
         book.read();
     }
     addNewBook(book);
+    getFromLibrary();
+    e.preventDefault();
 })
+//create new element and add to books
+const newElement = function(tagname, content) {
+    let element = document.createElement(`${tagname}`);
+    element.textContent = content;
+    books.appendChild(element);
+    return books;
+}
+//create checkbox for readingstate
+const readCheckBox = function(readState) {
+    let div = document.createElement("div");
+    let input = document.createElement("input");
+    div.innerText = "Read";
+    input.setAttribute("type", "checkbox");
+    if (readState) {
+        input.checked = true;
+    }
+    div.appendChild(input);
+    books.appendChild(div);
+}
+//Get books from library
+const getFromLibrary = function() {
+    if (library.length === 0) {
+    } else {
+        //Make sure screen is empty of books
+        while (books.firstChild) {
+            books.removeChild(books.lastChild);
+        }
+        library.forEach((bookObject) => {
+            //get bookobject info
+            [title, author, pages, readState] = bookObject.info();
+            //display books
+            newElement("div", title);
+            newElement("div", author);
+            newElement("div", pages);
+            readCheckBox(readState);
+        })
+    }   
+}
